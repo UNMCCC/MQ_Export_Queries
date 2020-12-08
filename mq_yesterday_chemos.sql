25cd3a4b25c2bb18a6e4c8c45a84a8553f01b973
Inigo San Gil <inigo.sangil@gmail.com>
2020-12-08 11:16:40 -0700

    Exclude bogus pats

--- mq_yesterday_chemos.sql
+++ mq_yesterday_chemos.sql
@@ -2,12 +2,12 @@
 SET NOCOUNT ON;
 Use Mosaiq;
 SELECT  
-    orc.orc_id,
-    ident.ida as MRN,
-    isNULL(QUOTENAME(MOSAIQ.dbo.fn_GetPatientName(orc.pat_id1, 'NAMELFM'),'"'),"") as PatName,
-    isNULL(QUOTENAME(convert(char(10), orc.Start_dtTm, 112),'"'),"") as AppDt,
-    isNULL(QUOTENAME(MOSAIQ.dbo.fn_GetStaffName(orc.Ord_Provider ,'NAMELF'),'"'),"") as Ordering_Prov,			
-    isNULL(QUOTENAME(rtrim(Drug.Drug_Label),'"'),"") as Drug_label
+   orc.orc_id,
+   ident.ida as MRN,
+   QUOTENAME(MOSAIQ.dbo.fn_GetPatientName(orc.pat_id1, 'NAMELFM'),'"') as PatName,
+   QUOTENAME(convert(char(10), orc.Start_dtTm, 112),'"') as AppDt,
+   QUOTENAME(MOSAIQ.dbo.fn_GetStaffName(orc.Ord_Provider ,'NAMELF'),'"') as Ordering_Prov,			
+   QUOTENAME(rtrim(Drug.Drug_Label),'"') as Drug_label
 --    ISNULL(QUOTENAME(Orc.Condition,'"'),'')as  comment
 
 FROM MOSAIQ.dbo.Orders orc
@@ -16,10 +16,13 @@ INNER JOIN MOSAIQ.dbo.PharmOrd RXO on orc.orc_set_id = rxo.ORC_set_ID and RXO.Ve
 LEFT JOIN MOSAIQ.dbo.Drug on  RXO.Req_Give_Code = Drug.drg_id
 
 WHERE 
-  orc.Start_dtTm >= dateadd(day,-1,getdate()) 
+  orc.Start_dtTm >= dateadd(day,-2,getdate()) 
  AND orc.Start_dtTm  <= getdate()
  AND orc.version = 0		       -- select tip records only 
  AND (  orc.Status_Enum = 5 OR orc.Status_enum = 18 
      OR orc.Status_Enum = 3 OR orc.Status_enum = 2 
      OR rxo.Status_Enum = 5 OR RXO.Status_enum= 18
      OR rxo.Status_Enum = 2 OR RXO.Status_enum= 3)
+ AND Ident.IDA not in ('','0000','00000000000','001123456','123456','123')
+ AND (Ident.IDA != ' ' OR Ident.IDA != '***************'  
+     OR Ident.IDA IS NOT NULL OR Ident.IDA NOT LIKE '%Do Not Use%' )   
